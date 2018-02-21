edd36164a7faed91e33d2a276064e42017e6a065
Hugo Ladeira de Freitas <HUGO_LADEIRA@hotmail.com>
2018-02-20 22:40:36 -0300

    commit inicial

---
+++ connectedin202/polls/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+import time
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    date = models.DateTimeField(default=time.strftime("%d/%m/%Y %H:%M:%S "))
---
+++ djangoversion.py
@@ -0,0 +1,4 @@
+import django
+# (2, 0, 2, 'final', 0)
+print(django.VERSION)
+# python -m django --version
